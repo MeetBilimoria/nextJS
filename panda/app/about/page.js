ed9fdaa4e5cfa618377329cbab4cb6995e2a075f
Meet Bilimoria <bilimoriameet007@gmail.com>
2023-12-04 19:05:39 +0530

    second comment

--- panda/app/about/page.js
+++ panda/app/about/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const about = () => {
   return (
     <div>
-      about
+       this is abouttttt not nowe
     </div>
   )
 }
--- panda/components/Navbar.jsx
+++ panda/components/Navbar.jsx
@@ -1,16 +1,22 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const Navbar = () => {
+  const router = useRouter()
   return (
     <nav>
         <ul>
             <div className="logo"><h1>Logo</h1></div>
+
             <div className="link-container">
                 <Link href='/' className='link'>Home</Link>
                 <Link href='/about'className='link'>About</Link>
                 <Link href='/portfolio'className='link'>Portfolio</Link>
             </div>
+            <button onClick={()=> router.push("contact")}> Lets talk</button>
         </ul>
     </nav>
   )
